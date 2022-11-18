b545c495e72b4cd5de72067c0e1027d4f67a3334
ancfoster <a.foster@outlook.com>
2022-11-18 14:28:42 +0000

    Update allowed local_hosts to include Heroku in settings.py

--- nextboat/settings.py
+++ nextboat/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'https://nextboat-ci.herokuapp.com']
 
 
 # Application definition
