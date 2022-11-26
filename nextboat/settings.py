9860613fc8d08e5ea28d7344a32fa5bbfeb52d08
ancfoster <a.foster@outlook.com>
2022-11-26 19:13:10 +0000

    Update settings.py to change AWS bucket URL

--- nextboat/settings.py
+++ nextboat/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'nextboat-ci-static'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.eu-west2.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
