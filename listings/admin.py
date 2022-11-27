4afebd155d03be0ccc4d6043127828e156e8d68d
ancfoster <a.foster@outlook.com>
2022-11-27 17:50:22 +0000

    Fix build issue caused by missing model import in admin.py

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Listings
+from .models import Listings, ListingMedia
+
 # Register your models here.
 admin.site.register(Listings)
 admin.site.register(ListingMedia)
