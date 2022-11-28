b15f22cb7f34b092bd1b4403aa463256a822103f
ancfoster <a.foster@outlook.com>
2022-11-28 19:00:32 +0000

    Create listing view in views.py

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Listings
 
 # Create your views here.
+class ListingsList(generic.ListView):
+    model = Listings
+    queryset = Listings.objects.all()
+    template_name = "listings/listings.html"
+
+
