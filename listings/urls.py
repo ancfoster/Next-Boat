151d260fa0c05531f6ad95eacf57db1b00c72bc3
ancfoster <a.foster@outlook.com>
2022-12-01 19:16:39 +0000

    Add listings details to views.py & urls.py

--- listings/urls.py
+++ listings/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('boats/', views.ListingsList.as_view(), name='boat_listings'),
+    # path('boats/<pk>', views.ListingDetails.as_view(), name='boat_listing_details'),
+    path('boats/<id>', views.ListingDetails.as_view(), name='boat_listing_details'),
     path('', views.home, name="home"),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
-from .models import Listings
+from django.views import generic, View
+from django.views.generic import DetailView
+from .models import Listings, ListingMedia
 from django.db.models import Q
 
 # Create your views here.
@@ -9,6 +10,27 @@ class ListingsList(generic.ListView):
     queryset = Listings.objects.filter(Q(listing_status='A') | Q(listing_status='P'))
     template_name = "listings/listings.html"
 
+
+# class ListingDetails(generic.DetailView):
+#     template_name = "listings/listing_details.html"
+#     queryset = Listings.objects.all()
+
+
+class ListingDetails(View):
+
+    def get(self, request, id, *args, **kwargs):
+        queryset = Listings.objects.all()
+        listing = get_object_or_404(queryset, id=id)
+        images = listing.listing_media.all()
+        
+        return render(
+            request,
+            "listings/listing_details.html",
+            {
+                "listing": listing,
+                "images": images
+            },
+        )
 
 def home(request):
     return render(request, 'listings/index.html')
