838251ea9b3b5ae1798c7ed7a0ae782c3cc1b222
ancfoster <a.foster@outlook.com>
2022-12-28 22:58:01 +0000

    Update base.html and urls.py with links in nav bar so they function

--- listings/urls.py
+++ listings/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('boats/', views.ListingsList.as_view(), name='boat_listings'),
-    # path('boats/<pk>', views.ListingDetails.as_view(), name='boat_listing_details'),
     path('boats/<id>', views.ListingDetails.as_view(), name='boat_listing_details'),
     path('', views.home, name="home"),
     path('create/', views.ListingCreate.as_view(), name="listingcreate"),
