6aa046a92abc8602baa4e9b8bf4965792cfd1185
ancfoster <a.foster@outlook.com>
2022-12-28 19:21:00 +0000

    Update apps list in settings.py

--- nextboat/settings.py
+++ nextboat/settings.py
@@ -45,8 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
     'storages',
     'listings',
 ]
