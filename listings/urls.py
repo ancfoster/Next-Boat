3679ab421b6a0192e517d5922c656953f894c8b1
ancfoster <a.foster@outlook.com>
2022-12-29 02:16:08 +0000

    Update urls.py create path with views.CreatingListing

--- listings/urls.py
+++ listings/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('boats/', views.ListingsList.as_view(), name='boat_listings'),
     path('boats/<id>', views.ListingDetails.as_view(), name='boat_listing_details'),
     path('', views.home, name="home"),
-    path('create/', views.ListingCreate.as_view(), name="listingcreate"),
+    path('create/', views.createListing, name='listing_create'),
 ]
