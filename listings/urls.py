eb93d9f501757114c9df774924b9d28959330812
ancfoster <a.foster@outlook.com>
2023-01-14 11:31:59 +0000

    Create my_listings.html template & update urls in  urls.py

--- listings/urls.py
+++ listings/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('boats/', views.ListingsList.as_view(), name='boat_listings'),
     path('boats/<id>', views.ListingDetails.as_view(), name='boat_listing_details'),
+    path('my-listings/', views.MyListings.as_view(), name='my_listings'),
     path('', views.home, name="home"),
     path('create/', views.createListing, name='listing_create'),
 ]
