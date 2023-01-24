d28925b8ad0769b9dfbd1e66171c4e5c8be674da
ancfoster <a.foster@outlook.com>
2023-01-24 23:15:07 +0000

    Update path to 404 in settings.py

--- nextboat/settings.py
+++ nextboat/settings.py
@@ -144,6 +144,6 @@ DEFAULT_FILE_STORAGE = 'nextboat.storage_backends.PublicMediaStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-handler404 = 'listings.views.view_404'
+handler404 = 'listings.views.view404'
 
 
