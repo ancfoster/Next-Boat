74a7acc8de93e7a3474b9aeb3b674c14045fa3cb
ancfoster <a.foster@outlook.com>
2023-01-24 12:26:56 +0000

    Add edit listing link to my_listings & update urls

--- listings/urls.py
+++ listings/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('boats/', views.ListingsList.as_view(), name='boat_listings'),
     path('boats/<id>', views.ListingDetails.as_view(), name='boat_listing_details'),
     path('boats/<id>/delete', views.ListingDelete.as_view(), name='listing_delete'),
+    path('boats/<id>/edit', views.EditListing, name='listing_edit'),
     path('my-listings/', views.MyListings.as_view(), name='my_listings'),
     path('', views.home, name="home"),
     path('create/', views.createListing, name='listing_create'),
