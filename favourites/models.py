380e8fd5256173dcbd16a091426819c480c7a58c
ancfoster <a.foster@outlook.com>
2023-01-25 20:47:46 +0000

    Create Favourite model in models.py & make migrate

--- favourites/models.py
+++ favourites/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from listings.models import Listings
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Favourites(models.Model):
+    listing = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name='favourite_listing')
+    
+    def __str__(self):
+        return f"{self.listing}"
