50a1db288383d6b84c482c16fea17112654fb2b2
ancfoster <a.foster@outlook.com>
2023-01-25 12:34:34 +0000

    Add DeleteImage to listing_edit_images & urls.py

--- listings/urls.py
+++ listings/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('boats/<id>', views.ListingDetails.as_view(), name='boat_listing_details'),
     path('boats/<id>/images', views.EditImages, name='listing_images'),
     path('boats/<id>/delete', views.ListingDelete.as_view(), name='listing_delete'),
+    path('boats/image/<id>/delete', views.DeleteImage.as_view(), name='delete_image'),
     path('boats/<id>/edit', views.EditListing, name='listing_edit'),
     path('my-listings/', views.MyListings.as_view(), name='my_listings'),
     path('', views.home, name="home"),
