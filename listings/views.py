b228da5ece86b32130b9c2c92731bf8722e206dd
ancfoster <a.foster@outlook.com>
2023-01-25 18:35:04 +0000

    Move 404 &500 template files to base, update views

--- listings/views.py
+++ listings/views.py
@@ -184,8 +184,8 @@ def compress_featured_image(image):
 
 
 #404 & 500 view
-def handler404(request, exception):
-    return render(request,'listings/404.html', status=404)
+# def handler404(request, exception):
+#     return render(request,'listings/404.html', status=404)
 
-def handler500(request):
-    return render(request,'listings/500.html', status=500)
+# def handler500(request):
+#     return render(request,'listings/500.html', status=500)
