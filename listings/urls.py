2ffe8386f261393db5f5e91f8f2c67d558f512ea
ancfoster <a.foster@outlook.com>
2023-01-27 07:50:38 +0000

    Update urls.py to pass PEP8 validation

--- listings/urls.py
+++ listings/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 
 urlpatterns = [
     path('boats/', views.ListingsList.as_view(), name='boat_listings'),
-    path('boats/<id>', views.ListingDetails.as_view(), name='boat_listing_details'),
+    path('boats/<id>', views.ListingDetails.as_view(),
+         name='boat_listing_details'),
     path('boats/<id>/images', views.EditImages, name='listing_images'),
-    path('boats/<id>/delete', views.ListingDelete.as_view(), name='listing_delete'),
-    path('boats/image/<id>/delete', views.DeleteImage.as_view(), name='delete_image'),
+    path('boats/<id>/delete', views.ListingDelete.as_view(),
+         name='listing_delete'),
+    path('boats/image/<id>/delete', views.DeleteImage.as_view(),
+         name='delete_image'),
     path('boats/<id>/edit', views.EditListing, name='listing_edit'),
     path('my-listings/', views.MyListings.as_view(), name='my_listings'),
     path('', views.home, name="home"),
