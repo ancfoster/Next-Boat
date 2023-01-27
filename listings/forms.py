4d5318daf5b466c7180135977681479247c806db
ancfoster <a.foster@outlook.com>
2023-01-27 07:23:42 +0000

    Fix forms.py to pass PEP8 validation

--- listings/forms.py
+++ listings/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Listings, ListingMedia
 
+
+# This is the form to create a new listing
 class ListingCreateForm(forms.ModelForm):
     class Meta:
         model = Listings
@@ -10,28 +12,35 @@ class ListingCreateForm(forms.ModelForm):
                     "enctype": "multipart/form-data"
                 }
             ),
-            "boat_feature_list": forms.HiddenInput(attrs={'required': 'false'}),
+            # Hidden inputs are used by create.js to relay values
+            "boat_feature_list": forms.HiddenInput(attrs={'required': 'false'}),  # noqa
             "type": forms.HiddenInput(),
             "condition": forms.HiddenInput(),
             "tax_paid": forms.HiddenInput(),
             "category": forms.HiddenInput(),
         }
         fields = "__all__"
-        exclude = ("created_by", "created_on", "last_modified", "listing_status", )
+        exclude = ("created_by", "created_on", "last_modified",
+                   "listing_status", )
 
+
+# This form edits listings & is the same as create except for no feaured_image
 class ListingEditForm(forms.ModelForm):
     class Meta:
         model = Listings
         widgets = {
-            "boat_feature_list": forms.HiddenInput(attrs={'required': 'false'}),
+            "boat_feature_list": forms.HiddenInput(attrs={'required': 'false'}),  # noqa
             "type": forms.HiddenInput(),
             "condition": forms.HiddenInput(),
             "tax_paid": forms.HiddenInput(),
             "category": forms.HiddenInput(),
         }
         fields = "__all__"
-        exclude = ("created_by", "created_on", "last_modified", "featured_image",)
+        exclude = ("created_by", "created_on", "last_modified",
+                   "featured_image",)
 
+
+# This form is for the gallery images, not multiple attr is true
 class ListingMediaForm(forms.ModelForm):
     class Meta:
         widgets = {
