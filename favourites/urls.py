1537587e6b761a158bd5dddb64219c9f7bbb873e
ancfoster <a.foster@outlook.com>
2023-01-27 08:19:05 +0000

    Update urls.py to pass PEP8 validation

--- favourites/urls.py
+++ favourites/urls.py
@@ -2,7 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('favourite/<id>', views.CreateDeleteFavourite, name='favourite_toggle'),
-    path('favourites/', views.ShowFavourites.as_view(), name='favourites_show'),
-    path('favourite/<id>/delete', views.FavouriteDelete.as_view(), name='favourite_delete'),
+    path('favourite/<id>', views.CreateDeleteFavourite,
+         name='favourite_toggle'),
+    path('favourites/', views.ShowFavourites.as_view(),
+         name='favourites_show'),
+    path('favourite/<id>/delete', views.FavouriteDelete.as_view(),
+         name='favourite_delete'),
 ]
