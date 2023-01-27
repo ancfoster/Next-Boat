498ee18779bda94d7bdee728407ddf162ce9b1ec
ancfoster <a.foster@outlook.com>
2023-01-27 08:27:22 +0000

    Update views.py to pass PEP8 validation

--- favourites/views.py
+++ favourites/views.py
@@ -21,14 +21,14 @@ from listings.models import Listings
 @login_required
 def CreateDeleteFavourite(request, id):
     listing_object = get_object_or_404(Listings, pk=id)
-    existing_favourite = Favourites.objects.filter(Q(favourite_created_by=request.user) & Q(listing=listing_object.pk))
+    existing_favourite = Favourites.objects.filter(Q(favourite_created_by=request.user) & Q(listing=listing_object.pk))  # noqa
     if existing_favourite.exists():
         existing_favourite.delete()
         return redirect('boat_listing_details', id=id)
     else:
         new_favourite = Favourites(
-            favourite_created_by = request.user,
-            listing = listing_object
+            favourite_created_by=request.user,
+            listing=listing_object
         )
         new_favourite.save()
         return redirect('boat_listing_details', id=id)
@@ -39,15 +39,17 @@ def CreateDeleteFavourite(request, id):
 class ShowFavourites(LoginRequiredMixin, generic.ListView):
     model = Favourites
     template_name = 'favourites/favourites.html'
+
     def get_queryset(self):
-        return Favourites.objects.filter(favourite_created_by=self.request.user)
+        return Favourites.objects.filter(favourite_created_by=self.request.user)  # noqa
 
 
-#This view deletes a user's listing from the favourites list
+# This view deletes a user's listing from the favourites list
 class FavouriteDelete(LoginRequiredMixin, DeleteView):
     template_name = 'favourites/favourite_delete.html'
+
     def get_object(self):
-        id= self.kwargs.get("id")
+        id = self.kwargs.get("id")
         return get_object_or_404(Favourites, id=id)
 
     def get_success_url(self):
