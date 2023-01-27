c1652465d5d58910178c0f552857ac189e632d81
ancfoster <a.foster@outlook.com>
2023-01-27 08:16:52 +0000

    Update models.py to pass PEP8 validation

--- favourites/models.py
+++ favourites/models.py
@@ -5,9 +5,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Favourites(models.Model):
-    listing = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name='favourite_listing')
+    listing = models.ForeignKey(Listings, on_delete=models.CASCADE,
+                                related_name='favourite_listing')
     favourite_created_on = models.DateTimeField(auto_now=True)
-    favourite_created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favourite_created_by', null=True)
-    
+    favourite_created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favourite_created_by', null=True)  # noqa
+
     def __str__(self):
         return f"{self.listing}"
